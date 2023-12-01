73d6aa0985bc26990a6d72817088d87c9b52ea48
Lorena Andrade <lorenaandrade@gmail.com>
2023-11-30 21:45:10 -0300

    subindo as classes

---
+++ Treino.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 
+{
+    public class Treino
+    {
+        public string Tipo { get; set; }
+        public string Objetivo { get; set; }
+        public List<Exercicio> ListaExercicios { get; set; }
+        public int DuracaoEstimadaMinutos { get; set; }
+        public DateTime DataInicio { get; set; }
+        public int VencimentoDias { get; set; }
+        public Treinador TreinadorResponsavel { get; set; }
+        public List<(Cliente, int)> ClientesAvaliacao { get; set; }
+    }
+}
